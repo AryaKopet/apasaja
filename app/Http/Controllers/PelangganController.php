46cf6df89422e5714617ab415a3e71c443f1bb02
AryaKopet <aryasokker23@gmail.com>
2024-12-03 23:19:02 +0700

    membuat model dan controller baru

---
+++ app/Http/Controllers/PelangganController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Pelanggan;
+
+class PelangganController extends Controller
+{
+    public function index()
+    {
+        $pelanggan = Pelanggan::all();
+        return view('dashboard', compact('pelanggan'));
+    }
+}
