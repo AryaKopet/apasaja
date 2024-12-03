c8795e67f0cddd311711d09bd8c51eec82eca8c1
AryaKopet <aryasokker23@gmail.com>
2024-12-03 23:22:22 +0700

    update routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\MaterialController;
+use App\Http\Controllers\PelangganController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,3 +13,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/dashboard', [AdminController::class, 'index'])->name('dashboard');
+Route::resource('admins', AdminController::class);
+Route::resource('materials', MaterialController::class);
+Route::resource('pelanggan', PelangganController::class);
